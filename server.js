95a8106febd93b7c43348afde88ed57adec62d29
Hilal Yilmaz <hilaltiftik@gmail.com>
2022-12-18 21:20:09 -0800

    not tip calculator but tip and percantage display

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
 app.get('/greeting', (req, res) => {
   res.send('Hello, stranger');
 });
@@ -9,6 +10,11 @@ app.get('/greeting', (req, res) => {
 app.get('/greeting/:name', (req, res) => {
     res.send(`What's up ` + req.params[`name`] + `? It's so great to see you!`);
 });
+
+
+app.get('/tip/:total/:tipPercantage', (req, res) => {
+    res.send(`yout total:  ` + req.params.total + `  your tip percentage:  ` + req.params.tipPercantage);
+  });
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`)
